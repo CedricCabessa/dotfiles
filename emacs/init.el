71e6e9841a24d24b11b446c10f1d5f2b21f0aec4
Cédric Cabessa <ced@ryick.net>
2024-07-25 14:26:32 +0200

    emacs stuff

--- emacs/init.el
+++ emacs/init.el
@@ -44,8 +44,17 @@
 (setq-default show-trailing-whitespace t)
 (server-start)
 
-;; search case insensitive
+;; case insensitive
 (setq case-fold-search t)
+(setq read-buffer-completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+(setq vc-follow-symlinks t)
+
+(setq select-enable-primary t)
+
+(setq recentf-max-saved-items 1000)
+
 
 (defun match-paren (arg)
   "Go to the matching paren if on a paren; otherwise insert %."
@@ -80,12 +89,11 @@
   (completion-category-overrides '((file (styles basic partial-completion))))
   )
 ; buffer switch
+(bind-key "C-x b" 'helm-mini)
 (use-package bufler
   :ensure t
   :diminish
-  ;:init (bufler-mode)
   :bind
-  ("C-x b" . 'bufler-switch-buffer)
   ("C-x C-b" . 'bufler)
   )
 
@@ -111,6 +119,15 @@
   (doom-themes-org-config)
   )
 
+;; all-the-icons-install-fonts
+(use-package all-the-icons
+  :ensure t
+  )
+(use-package all-the-icons-dired
+  :ensure t
+  :defer
+  :hook ((dired-mode . all-the-icons-dired-mode))
+  )
 
 (use-package eglot
   :ensure t
@@ -139,11 +156,25 @@
 					  (pyvenv-restart-python)))
   :hook (python-mode . pyvenv-mode)
   )
-(defun ced/pyvenv-show()
-  "show current python venv"
-    (interactive)
-    (print pyvenv-virtual-env-name)
-    )
+
+(use-package python-pytest
+  :ensure t
+  )
+
+(use-package with-venv
+  :ensure t
+  ) ; needed for dap-python
+
+(use-package dap-mode
+  :ensure t
+  :config
+  (setq dap-python-debugger 'debugpy)
+  (require 'dap-mode)
+  (require 'dap-python)
+  (defun dap-python--pyenv-executable-find (command)
+    (with-venv (executable-find "python")))
+  )
+
 
 (use-package blacken
   :ensure t
@@ -156,7 +187,15 @@
   :config
   ;; comment to disable rustfmt on save
   (setq rustic-format-on-save t)
-)
+  )
+
+(use-package lsp-metals ; scala
+  :ensure t
+  )
+
+(use-package typescript-mode
+  :ensure t
+  )
 
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
@@ -184,11 +223,20 @@
   (company-quickhelp-mode 1)
   )
 
+(use-package hl-todo
+  :ensure t
+  :hook
+  (prog-mode . hl-todo-mode)
+  )
 
 (use-package magit
   :ensure t
   :init
   (setq git-commit-max-length 50)
+  )
+(use-package forge
+  :after magit
+  :ensure t
   )
 
 (use-package git-link
@@ -197,7 +245,23 @@
   (setq git-link-open-in-browser t)
   )
 
+(use-package restclient
+  :ensure t
+  :mode (("\\.http\\'" . restclient-mode))
+  :init (require 'restclient-jq)
+  :config
+  (setq restclient-inhibit-cookies t)
+  )
 
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
+  :ensure t
+  :bind (:map copilot-completion-map
+	      ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+	      )
+  :hook ((prog-mode . copilot-mode))
+  )
 
 (use-package org
   :ensure t
@@ -206,7 +270,15 @@
   (setq org-log-into-drawer t)
   (setq org-todo-keywords (quote ((sequence "TODO(!)" "DONE(!)"))))
   (setq org-startup-folded t)
-  (add-hook 'org-mode-hook 'org-indent-mode)
+  (setq org-hide-emphasis-markers t)
+  (dolist (face '((org-level-1 . 1.5)
+                  (org-level-2 . 1.4)
+                  (org-level-3 . 1.3)
+                  (org-level-4 . 1.2)
+                  (org-level-5 . 1.1)
+		  ))
+    (set-face-attribute (car face) nil :height (cdr face)))
+  (add-hook 'org-mode-hook 'my-org)
   ;; display 7 days in overview
   (setq org-agenda-ndays 7)
   ;; start calendar on the current day
@@ -214,21 +286,11 @@
   ;; first day of week is monday
   (setq calendar-week-start-day 1)
   (setq org-capture-templates
-	(quote
-	 (("j" "Journal" entry
-	   (file "~/org/journal.org")
-	   "* %u %?
-%F
-" :prepend t)
-     ("w" "work achievement" entry
-      (file "~/org/work/journal.org")
-      "* %u %?
-" :prepend t)
-     ("i" "inbox" entry
+   '(("i" "inbox" entry
       (file "~/org/inbox.org")
-      "* TODO %?\n %U
-" :prepend t)
-     )))
+      "* %U %k\12%?" :prepend t)))
+  (setq org-refile-targets '((org-agenda-files :maxlevel . 2)))
+
   (setq org-agenda-files
 	(seq-filter
 	 (lambda (x) (and (not (string-match-p (regexp-quote "/archive/") x))
@@ -252,14 +314,34 @@
    'org-babel-load-languages
    '((shell . t)
      (python . t)
+     (restclient . t)
      ))
   (setq org-confirm-babel-evaluate nil)
+  (setq org-startup-with-inline-images t)
+  (setq org-toggle-inline-images t)
+  (setq org-agenda-prefix-format
+	'((agenda . " %i %-12:c%?-12t% s %b")
+	(todo . " %i %-12:c%?-12t% s %b")
+	(tags . " %i %-12:c%?-12t% s %b")
+	(search . " %i %-12:c%?-12t% s %b"))
+	)
   :bind
   ("C-c a" . 'org-agenda)
   )
 
+(defun my-org()
+  (org-indent-mode 1)
+)
+
+(use-package org-roam
+  :ensure t
+  :config
+  (setq org-roam-directory "~/org/roam/")
+)
+
 (use-package org-roam-ui
   :ensure t
+  :after org-roam
   )
 
 (use-package org-super-links
@@ -287,7 +369,7 @@
   (deft-recursive t)
   (deft-use-filter-string-for-filename t)
   (deft-default-extension "org")
-  (deft-directory org-roam-directory)
+  (deft-directory "~/org/roam")
   )
 
 (use-package org-super-agenda
@@ -300,15 +382,53 @@
   :mode ("*\\.md" . gfm-mode)
   :config
   (setq markdown-css-paths '("https://markdowncss.github.io/retro/css/retro.css"))
+  (setq markdown-fontify-code-blocks-natively t)
+  (setq markdown-enable-math t)
   )
 ;; render ghm page
 (use-package grip-mode
   :ensure t
   )
 
+(use-package visual-fill-column
+  :ensure t
+  )
+(use-package org-present
+  :ensure t
+  :init
+  (add-hook 'org-present-mode-hook 'ced/org-present-start)
+  (add-hook 'org-present-mode-quit-hook 'ced/org-present-end)
+  )
+
+;; incompatible org version??
+
+;; (use-package org-excalidraw
+;;   :straight (:type git :host github :repo "wdavew/org-excalidraw")
+;;   :config
+;;   (setq org-excalidraw-directory "~/org/excalidraw")
+;;   (org-excalidraw-initialize)
+;; )
+(setq org-excalidraw-directory "~/org/excalidraw")
+(load-file "~/workdir/elisp/org-excalidraw.el")
+(org-excalidraw-initialize)
+
 ;; text
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'text-mode-hook 'auto-fill-mode)
+(add-hook 'text-mode-hook 'my-text)
+;(add-hook 'text-mode-hook 'texfrag-global-mode)
+;; reload iimage on every change excalidraw?
+;; https://emacs.stackexchange.com/questions/3302/live-refresh-of-inline-images-with-org-display-inline-images
+
+(defun my-text()
+  (setq-default fill-column 120)
+  (flyspell-mode 1)
+  (auto-fill-mode 1)
+  (iimage-mode 1)
+  (setq-default visual-fill-column-width 120)
+  (setq-default visual-fill-column-center-text t)
+  (visual-fill-column-mode 1)
+  (visual-line-mode 1)
+)
+(load-file "~/workdir/dotfiles/emacs/ced.el")
 
 
 (custom-set-variables
@@ -316,9 +436,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ignored-local-variable-values '((pyvenv-workon . vault-api-gateway-k6r7IIeG)))
+ '(org-agenda-files
+   '("/home/ccabessa/org/perso/ion.org" "/home/ccabessa/org/perso/jdr.org" "/home/ccabessa/org/perso/perso.org" "/home/ccabessa/org/perso/ref.org" "/home/ccabessa/org/work/clock.org" "/home/ccabessa/org/work/interviews.org" "/home/ccabessa/org/work/ledger.org" "/home/ccabessa/org/inbox.org"))
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 1.5 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(package-selected-packages
-   '(consult company-quickhelp company-quickhelp-mode company vertico orderless marginalia doom-themes doom-modeline))
- '(safe-local-variable-values '((pyvenv-workon . ledger-vault-api-AAYNituf))))
+   '(vertico marginalia orderless bufler doom-modeline doom-themes all-the-icons all-the-icons-dired eglot pyvenv blacken rustic consult company company-quickhelp hl-todo magit forge git-link org org-roam-ui org-super-links helm-org-rifle deft org-super-agenda markdown-mode grip-mode visual-fill-column ruff-format restclient python-pytest projectile org-present yaml-mode restclient-jq jq-mode lsp-metals typescript-mode copilot with-venv pocket-reader ob-restclient))
+ '(safe-local-variable-values '((pyvenv-workon . ledger-vault-api-AAYNituf)))
+ '(typescript-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
